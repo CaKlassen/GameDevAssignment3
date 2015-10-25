28ea93f3211f00e87c0f9f42781048777a4104de
Chris Klassen <ca.klassen95@gmail.com>
2015-10-24 20:37:29 -0700

    Made the game beautiful, added proper menu start.

--- Source/Assignment3/Assignment3/Assignment3/Scenes/MenuScene.cs
+++ Source/Assignment3/Assignment3/Assignment3/Scenes/MenuScene.cs
@@ -14,6 +14,8 @@ namespace Assignment3.Scenes
 {
     public class MenuScene : Scene
     {
+        private SpriteFont font;
+
         public MenuScene()
         {
 
@@ -21,21 +23,41 @@ namespace Assignment3.Scenes
 
         public override void onLoad(ContentManager content)
         {
-
+            font = content.Load<SpriteFont>("MenuFont");
         }
 
         public override void update(GameTime gameTime, GamePadState gamepad, KeyboardState keyboard)
         {
-            if (keyboard.IsKeyDown(Keys.Enter))
+            if (keyboard.IsKeyDown(Keys.D1) || gamepad.IsButtonDown(Buttons.A))
             {
                 // Go to the maze
+                MazeCommunication.setDifficulty(MazeDifficulty.EASY);
+                BaseGame.instance.changeScene(SceneType.MAZE);
+            }
+            else if (keyboard.IsKeyDown(Keys.D2) || gamepad.IsButtonDown(Buttons.B))
+            {
+                // Go to the maze
+                MazeCommunication.setDifficulty(MazeDifficulty.MEDIUM);
+                BaseGame.instance.changeScene(SceneType.MAZE);
+            }
+            else if (keyboard.IsKeyDown(Keys.D3) || gamepad.IsButtonDown(Buttons.X))
+            {
+                // Go to the maze
+                MazeCommunication.setDifficulty(MazeDifficulty.HARD);
                 BaseGame.instance.changeScene(SceneType.MAZE);
             }
         }
 
         public override void draw(SpriteBatch sb)
         {
+            sb.Begin();
 
+            sb.DrawString(font, "MAZE GAME!!!!", new Vector2(100, 100), Color.White);
+            sb.DrawString(font, "1/A: Easy Maze", new Vector2(100, 200), Color.White);
+            sb.DrawString(font, "2/B: Medium Maze", new Vector2(100, 240), Color.White);
+            sb.DrawString(font, "3/X: Hard Maze", new Vector2(100, 280), Color.White);
+
+            sb.End();
         }
     }
 }
